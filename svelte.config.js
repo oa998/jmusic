25bed1b361a2016a214aaa2a58536a69d7c079ec
zzz sss <onlineaccounts998@gmail.com>
2024-04-26 19:03:01 -0400

    auto commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,21 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	adapter: adapter({
+		// default options are shown. On some platforms
+		// these options are set automatically — see below
+		pages: 'build',
+		assets: 'build',
+		fallback: undefined,
+		precompress: false,
+		strict: true
+	}),
+	alias: {
+		// this will match a file
+		'$components/*': 'src/components',
+		'$stores/*': 'src/stores',
+		'$lib/*': 'src/lib'
+	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
